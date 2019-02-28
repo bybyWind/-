55dfd4d3c9204cc875b6df3f039b0f8050fedc58
Chen Jing Feng <ChenJFChn@163.com>
2019-02-28 12:47:38 +0800

    Initial Commit

---
+++ 仿朴朴首页/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  仿朴朴首页
+//
+//  Created by 陈经风 on 2019/2/28.
+//  Copyright © 2019 funnyS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 仿朴朴首页/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  仿朴朴首页
+//
+//  Created by 陈经风 on 2019/2/28.
+//  Copyright © 2019 funnyS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
